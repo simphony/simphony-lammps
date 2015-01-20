3b3b14b74ad82846b738b15ea04de04f6ea1e1fe
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-01-20 17:56:41 +0100

    change handling of pair style
    now form of input is different and update logic
    so that we can handle hybrid/overlay of pair styles

--- simlammps/config/pair_style.py
+++ simlammps/config/pair_style.py
@@ -2,12 +2,12 @@ import yaml
 
 from simphony.core.cuba import CUBA
 
-# here is the info to create the different handlers
-# TODO add more handlers for the different types
-_pair_style_handlers = {"lj": {"pair_style": "lj/cut",
-                               "required_global_params": ["global_cutoff"],
-                               "required_pair_params":
-                               ["epsilon", "sigma", "cutoff"]}}
+# here is the info to create the different pair info handlers
+# TODO add more pair styles for the different types
+_supported_pair_styles = {"lj": {"pair_style": "lj/cut",
+                                 "required_global_params": ["global_cutoff"],
+                                 "required_pair_params":
+                                 ["epsilon", "sigma", "cutoff"]}}
 
 
 class PairStyle(object):
@@ -27,7 +27,7 @@ class PairStyle(object):
         """
         self._global = ""
         self._pair_coefs = ""
-        self._handler = self._create_handler(CM)
+        self._pair_infos = self._create_pair_infos(CM)
 
     def get_global_config(self):
         """ Returns global configuration text
@@ -39,10 +39,15 @@ class PairStyle(object):
         An empty string is returned if there is no pair style
         information
         """
-        if self._handler:
-            return "pair_style {} {}".format(
-                self._handler.pair_style,
-                " ".join(map(str, self._handler.get_global_parameters())))
+        if self._pair_infos:
+            if len(self._pair_infos) == 1:
+                pair_info = self._pair_infos[0]
+                return "pair_style {} {}".format(
+                    pair_info.pair_style,
+                    " ".join(map(str, pair_info.global_params)))
+            else:
+                # TODO do hybrid/overlay style
+                raise NotImplementedError
         else:
             return ""
 
@@ -50,11 +55,16 @@ class PairStyle(object):
         """ Returns global configuration text
 
         """
-        if self._handler:
+        if self._pair_infos:
             coeffs = ""
-            for pair_params in self._handler.iter_pair_params():
-                coeffs = coeffs + "pair_coeff " + \
-                    " ".join(map(str, pair_params)) + "\n"
+            for pair_info in self._pair_infos:
+                for pair, params in pair_info.pair_params.iteritems():
+                    # first list pair info
+                    result = list(map(int, pair))
+                    # then all params
+                    result += params
+                    coeffs = coeffs + "pair_coeff " + \
+                        " ".join(map(str, result)) + "\n"
             coeffs = coeffs + "\n"
             return coeffs
         else:
@@ -62,98 +72,92 @@ class PairStyle(object):
 
     # Private methods #######################################################
 
-    def _create_handler(self, CM):
-        """ Creates appropriate handler(s) from information stored in the
-            CM.
+    def _create_pair_infos(self, CM):
+        """ Creates appropriate pair info handler(s) from information
+            stored in the CM.
 
         """
+        styles = []
         if CUBA.PAIR_STYLE in CM and CM[CUBA.PAIR_STYLE]:
+            keywords = yaml.safe_load(CM[CUBA.PAIR_STYLE])
             my_pair_style = CM[CUBA.PAIR_STYLE]
-            if my_pair_style in _pair_style_handlers:
-                handler_info = _pair_style_handlers[my_pair_style]
-                req_global = handler_info["required_global_params"]
-                req_params = handler_info["required_pair_params"]
-                return PairStyleHandler(CM,
-                                        pair_style=handler_info["pair_style"],
-                                        required_global_params=req_global,
-                                        required_pair_params=req_params)
+
+            for key in keywords:
+                my_pair_style = key
+                parameters = keywords[key]
+                if my_pair_style in _supported_pair_styles:
+                    handler_info = _supported_pair_styles[my_pair_style]
+                    req_global = handler_info["required_global_params"]
+                    req_params = handler_info["required_pair_params"]
+                    global_params = self._get_global_params(
+                        parameters, req_global)
+                    pair_params = self._get_pair_params(parameters, req_params)
+                    styles.append(
+                        PairStyleInfo(pair_style=handler_info["pair_style"],
+                                      global_params=global_params,
+                                      pair_params=pair_params))
+                else:
+                    raise Exception(
+                        "Unsupported pair style: {}".format(my_pair_style))
+        return styles
+
+    def _get_global_params(self, keywords, required_global_params):
+        global_params = []
+        missing_params = []
+        for req in required_global_params:
+            if req in keywords:
+                global_params.append(keywords[req])
             else:
-                raise Exception(
-                    "Unsupported pair style: {}".format(my_pair_style))
-        return None
+                missing_params.append(req)
+
+        if missing_params:
+            msg = "The following global parameters are missing: "
+            msg += ', '.join(missing_params)
+            raise Exception(msg)
+
+        return global_params
+
+    def _get_pair_params(self, keywords, required_pair_params):
+        pair_params = {}
+
+        parameter_keywords = keywords["parameters"]
+        for params in parameter_keywords:
+            pair = None
+            if "pair" in params:
+                pair = tuple((params["pair"]))
+            else:
+                raise Exception("Pair information is missing")
+
+            pair_dict = []
+            missing_params = []
+            for req in required_pair_params:
+                if req in params:
+                    pair_dict.append(params[req])
+                else:
+                    missing_params.append(req)
+                    msg = "The pair ({}) is missing {}".format(pair, req)
+                    raise Exception(msg)
+
+            if missing_params:
+                msg = "The pair ({}) is missing the following {}".format(
+                    pair, req)
+                msg += ', '.join(missing_params)
+                raise Exception(msg)
+
+            pair_params[pair] = pair_dict
+        return pair_params
 
 
-class PairStyleHandler(object):
-    """ A pair style handler which knows its required global and pair-specific
+class PairStyleInfo(object):
+    """ A pair style info which knows its all required global and pair-specific
     parameters
 
     """
 
     def __init__(self,
-                 CM, pair_style,
-                 required_global_params, required_pair_params):
-        self._required_global_params = required_global_params
-        self._required_pair_params = required_pair_params
+                 pair_style,
+                 global_params,
+                 pair_params):
         self.pair_style = pair_style
-        self._global_params = None
-        self._pair_params = None
-        if CUBA.PAIR_STYLE in CM:
-            if CUBA.PAIR_STYLE_PARAMETERS in CM:
-                keywords = yaml.safe_load(CM[CUBA.PAIR_STYLE_PARAMETERS])
-                self._global_params = self._get_global_params(keywords)
-                self._pair_params = self._get_pair_params(keywords)
-            else:
-                raise Exception(
-                    "CUBA.PAIR_STYLE_PARAMETERS was not set in the CM")
-
-    def _get_global_params(self, keywords):
-        global_params = {}
-        for params in keywords:
-            for req in self._required_global_params:
-                if req in params:
-                    global_params[req] = params[req]
-
-        missing_reqs = list(self._required_global_params)
-        for key in global_params:
-            while key in missing_reqs:
-                missing_reqs.remove(key)
-        if missing_reqs:
-            msg = ("Missing the following required "
-                   "pair style parameters: {}").format(missing_reqs)
-            raise Exception(msg)
-        return global_params
-
-    def _get_pair_params(self, keywords):
-        pair_params = {}
-        for params in keywords:
-            if "pair" in params:
-                pair = tuple((params["pair"]))
-                pair_dict = {}
-                for req in self._required_pair_params:
-                    if req in params:
-                        pair_dict[req] = params[req]
-                    else:
-                        msg = "The pair ({}) is missing {}".format(pair, req)
-                        raise Exception(msg)
-                pair_params[pair] = pair_dict
-        return pair_params
-
-    def get_global_parameters(self):
-        """Returns list of values of all the required global parameters
-
-        """
-
-        params = []
-        for req in self._required_global_params:
-            params.append(self._global_params[req])
-        return params
-
-    def iter_pair_params(self):
-        """Returns list of global parameters
-
-        """
-        for pair in self._pair_params:
-            result = list(map(int, pair))
-            for req in self._required_pair_params:
-                result.append(self._pair_params[pair][req])
-            yield result
+        self.global_params = global_params
+        self.pair_params = pair_params
