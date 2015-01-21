dbbe8df6192a393f460e495352d06b906dbda792
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-01-21 17:56:32 +0100

    Revert "Revert "WIP:  Feature particles in Lammps""
    This reverts commit 44ac50f12875540d54a12ddb3ab08797887838d5.

---
+++ examples/flow/flow_pois.py
@@ -0,0 +1,99 @@
+from __future__ import print_function
+import sys
+from sets import Set
+
+from simphony.core.cuba import CUBA
+
+from simlammps.lammps_wrapper import LammpsWrapper
+from simlammps.lammps_process import LammpsProcess
+from simlammps.io.lammps_data_file_parser import LammpsDataFileParser
+from simlammps.io.lammps_simple_data_handler import LammpsSimpleDataHandler
+from simlammps.dummy import LammpsDummyConfig
+
+# This example is to run a lammps flow
+# problem. This example is to corresponds to the
+# lammps example 'in.flow.pois'
+# In this script, we do the following:
+# (1) run lammps with 'in.flow.pois'
+# (2) run lammps with SimPhoNy (with a similar configuration)
+# (3) compare results
+
+
+def get_set(file_name):
+    """ Get set which each entry describes a point
+
+    compare what was produced in each file
+    each file should have particles with matching
+    positions and velocities.  however, the id's
+    can be totally different.  Here, we just collect
+    all the positions and check if they match
+    """
+
+    handler = LammpsSimpleDataHandler()
+    parser = LammpsDataFileParser(handler=handler)
+
+    parser.parse(file_name)
+    atoms = handler.get_atoms()
+    vel = handler.get_velocities()
+
+    positions_velocities = Set()
+    for index, atom in atoms.iteritems():
+        positions_velocities.add(
+            str(atom[0]) + str(atom[1:4]) + str(vel[index]))
+    return positions_velocities
+
+# ----------------------------
+# (1) run lammps with in.flow.pois
+# ----------------------------
+lammps = LammpsProcess()
+command = ""
+with open('examples/flow/in.flow.pois', 'r') as script:
+        command = script.read()
+lammps.run(command)
+
+# ----------------------------
+# (2) run lammps with SimPhony
+# ----------------------------
+
+wrapper = LammpsWrapper()
+wrapper.CM[CUBA.NUMBEROF_TIME_STEPS] = 10000
+wrapper.CM[CUBA.TIME_STEP] = 0.003
+
+for i, pc in LammpsDummyConfig.get_particle_containers().iteritems():
+    wrapper.add_particle_container(str(i), pc)
+
+wrapper.run()
+
+# ----------------------------
+# (3) compare results
+# ----------------------------
+# TODO this should be done another way.
+#
+# data.lammps should have the final result of the simphony-based
+# calculation while final_data.lammps should have the final result
+# using in.flow.pois script
+
+simphony_positions = get_set('final_data.lammps')
+script_positions = get_set('data.lammps')
+
+missing_point = False
+msg = ""
+for coord in simphony_positions:
+    if coord not in script_positions:
+        missing_point = True
+if missing_point:
+    msg = """ Results did not match up.
+ Corresponding particle(s) not found. See
+ simphony_coordinates.txt and script_coordinates.txt """
+    with open('script_coordinates.txt', 'w') as f:
+        for c in sorted(script_positions):
+            f.write(c + "\n")
+    with open('simphony_coordinates.txt', 'w') as f:
+        for c in sorted(simphony_positions):
+            f.write(c + "\n")
+
+if msg:
+    sys.exit(msg)
+else:
+    print ("Results from a simphony-controlled lammps\
+ succesfully matched up with results produced by lammps")
---
+++ simlammps/tests/test_particle_container.py
@@ -0,0 +1,91 @@
+import unittest
+from simphony.cuds.particles import Particle
+from simlammps.lammps_wrapper import LammpsWrapper
+from simlammps.dummy import LammpsDummyConfig
+
+
+def _get_particle(particle_container):
+    for p in particle_container.iter_particles():
+        return p
+    else:
+        raise Exception("could not find a particle to test with")
+
+
+class TestLammpsParticleContainer(unittest.TestCase):
+
+    def setUp(self):
+
+        self.wrapper = LammpsWrapper()
+
+        # configuration is being done by dummy class
+        # the wrapper is properly configured with
+        # CM/SP/BC and given particles
+        LammpsDummyConfig.configure_wrapper(self.wrapper)
+
+        # keep track of first wrapper-based particle container
+        # and the particle ids that it contains
+        pcs = [pc for _, pc in self.wrapper.iter_particle_containers()]
+        self.pc = pcs[0]
+        self.particle_ids_in_pc = []
+        for p in pcs[0].iter_particles():
+            self.particle_ids_in_pc.append(p.id)
+
+    def test_update_non_existing_particle(self):
+        with self.assertRaises(KeyError):
+            p = Particle(id=100000000, coordinates=(0.0, 0.0, 0.0))
+            self.pc.update_particle(p)
+
+    def test_get_non_existing_particle(self):
+        with self.assertRaises(KeyError):
+            self.pc.get_particle(100000000)
+
+    def test_update_particle(self):
+        particle = _get_particle(self.pc)
+        particle.coordinates = (42.0, 42.0, 42.0)
+        self.pc.update_particle(particle)
+        updated_p = self.pc.get_particle(particle.id)
+
+        self.assertEqual(particle.coordinates, updated_p.coordinates)
+
+    def test_add_particle_without_id(self):
+        p = Particle(coordinates=(0.0, 2.5, 0.0))
+        id = self.pc.add_particle(p)
+        added_p = self.pc.get_particle(id)
+        self.assertEqual(p.coordinates, added_p.coordinates)
+
+    def test_delete_particle(self):
+        removed_particle = _get_particle(self.pc)
+        self.pc.remove_particle(removed_particle.id)
+
+        # check that it was removed
+        with self.assertRaises(KeyError):
+            self.pc.get_particle(removed_particle.id)
+
+        self.wrapper.run()
+
+        # check that it stayed removed
+        with self.assertRaises(KeyError):
+            self.pc.get_particle(removed_particle.id)
+
+    def test_iter_particles(self):
+        iterated_ids = []
+        for particle in self.pc.iter_particles():
+            iterated_ids.append(particle.id)
+
+        self.assertEqual(set(self.particle_ids_in_pc), set(iterated_ids),
+                         'Error: incorrect iteration!')
+
+        # make different list of ids to test with
+        ids = self.particle_ids_in_pc
+        ids = ids[0:3]
+
+        iterated_ids = []
+        for particle in self.pc.iter_particles(ids):
+            iterated_ids.append(particle.id)
+
+        self.assertEqual(set(ids), set(iterated_ids),
+                         'Error: incorrect iteration! {0}---\n{1}'
+                         .format(ids, iterated_ids))
+
+if __name__ == '__main__':
+    unittest.main()
