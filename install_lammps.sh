6de52f81c9fbd8026ad57f7102e096f5f65d1fe6
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-02-06 14:11:52 +0100

    fix lammps install script

--- install_lammps.sh
+++ install_lammps.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # checkout a recent stable version (from 28 June 2014)
-cd mylammps/src && git checkout r12164
 git clone git://git.lammps.org/lammps-ro.git mylammps
+pushd mylammps && git checkout r12164 && popd
 
-#  (1) build lammps as shared library
-# make shared library for python
-make makeshlib && make -f Makefile.shlib ubuntu_simple
+# (1) build lammps as shared library
+#  and install LAMMPS python wrapper
+pushd mylammps/src 
+make makeshlib 
+make -f Makefile.shlib ubuntu_simple
 cd ../python && sudo python install.py /usr/lib/ $VIRTUAL_ENV/lib/python2.7/site-packages/
+popd
 
-#  (2) and install LAMMPS python wrapper
-# make lammps executable and add to path
-cd ../src
+# (2) make lammps executable and add to path
+pushd mylammps/src 
 make ubuntu_simple
 ln -s lmp_ubuntu_simple lammps
 export PATH=$PWD:$PATH
-cd ../../
+popd
