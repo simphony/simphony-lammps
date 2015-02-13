babc465af2b4bfa126bb53d6191b8dcefff34f1f
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-02-13 16:15:27 +0100

    build only lammps executable
    not using LAMMPS-provided wrapper yet, so there is
    no need to build it. also, just creating symbolic
    link to lammps at the travis-ci's working directory

--- install_lammps.sh
+++ install_lammps.sh
@@ -4,17 +4,8 @@
 git clone git://git.lammps.org/lammps-ro.git mylammps
 pushd mylammps && git checkout r12164 && popd
 
-# (1) build lammps as shared library
-#  and install LAMMPS python wrapper
-pushd mylammps/src 
-make makeshlib 
-make -f Makefile.shlib ubuntu_simple
-cd ../python && sudo python install.py /usr/lib/ $VIRTUAL_ENV/lib/python2.7/site-packages/
-popd
-
-# (2) make lammps executable and add to path
+# build lammps executable 
 pushd mylammps/src 
 make ubuntu_simple
-ln -s lmp_ubuntu_simple lammps
-export PATH=$PWD:$PATH
 popd
+ln -s mylammps/src/lmp_ubuntu_simple lammps
