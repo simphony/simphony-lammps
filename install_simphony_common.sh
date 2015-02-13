9e138ce225849576a0dba0a74274c3e4adea1618
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-02-13 11:07:39 +0100

    fix travis

--- install_simphony_common.sh
+++ install_simphony_common.sh
@@ -4,7 +4,6 @@
 #  -edit cuba list
 #  -install simphony-common
 
-
 sudo apt-get install libhdf5-serial-dev
 
 git clone git://github.com/simphony/simphony-common.git
@@ -13,7 +12,7 @@ git clone git://github.com/simphony/simphony-common.git
 cat additional_cuba.yml >> simphony-common/simphony/core/cuba.yml
 
 pushd simphony-common
-pip install numexpr cython==0.20
+pip install numpy numexpr cython==0.20
 pip install -r requirements.txt
 python simphony/scripts/cuba_generate.py python simphony/core/cuba.yml simphony/core/cuba.py
 sudo python setup.py install
