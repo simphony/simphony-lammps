f2efa69ca0d012f4c0268fcf33057645a924cf3a
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-03-10 10:40:29 +0100

    add comment on why we are installing in this manner

--- install_simphony_common.sh
+++ install_simphony_common.sh
@@ -5,5 +5,9 @@
 set -e
 sudo apt-get install libhdf5-serial-dev
 
+# (1) Install cython and numexpr externally because the requirements does
+#     work with them
+# (2) Pytables breaks with latest Cython
+#     see https://github.com/PyTables/PyTables/issues/388
 pip install numpy numexpr cython==0.20
 pip install -e git://github.com/simphony/simphony-common.git#egg=simphony
