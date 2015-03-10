b6165f67535eba6d298f632f0a689b5b8694d561
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-03-10 10:40:42 +0100

    install tables
    We are not using H5I0 but we are using
    simphony-common.simphony.testing.abc_check_particle_containers
    which uses testing.utils which requires the tables package

--- install_simphony_common.sh
+++ install_simphony_common.sh
@@ -10,4 +10,8 @@ sudo apt-get install libhdf5-serial-dev
 # (2) Pytables breaks with latest Cython
 #     see https://github.com/PyTables/PyTables/issues/388
 pip install numpy numexpr cython==0.20
+# installing tables as although we aren't using H5I0, we
+# are using simphony-common.simphony.testing.abc_check_particle_containers
+# which uses testing.utils which does utilize H5IO to get types of CUBA
+pip install tables
 pip install -e git://github.com/simphony/simphony-common.git#egg=simphony
