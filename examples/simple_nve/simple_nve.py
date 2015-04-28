2f4dff07049ff709ff36305781f89ab1ef87373c
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-04-28 16:49:30 +0200

    fix spelling

--- examples/simple_nve/simple_nve.py
+++ examples/simple_nve/simple_nve.py
@@ -28,7 +28,7 @@ from simphony.cuds.particles import Particle, Particles
 def write_file(particles, file_format, file_name):
     if file_format == "EXYZ":
         f = open(file_name+".exyz", "w")
-        # until we have number_of_partucles method or similar...
+        # until we have number_of_particles method or similar...
         particles_size = 0
         for p in particles.iter_particles():
             particles_size += 1
