38ecde1e52750ce5cfeffc4a58ca600c4e08bc6b
itziakos <ioannist@enthought.com>
2015-05-20 19:10:40 +0100

    update git clone to only get the desired branch

--- install_external.sh
+++ install_external.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 # checkout a recent stable version (from 9 Dec 2014)
-git clone git://git.lammps.org/lammps-ro.git mylammps
-pushd mylammps && git checkout r12824 && popd
+git clone --branch r12824 --depth 1 git://git.lammps.org/lammps-ro.git mylammp
 
-# build lammps executable 
-pushd mylammps/src 
+# build lammps executable
+pushd mylammps/src
 make -j 2 ubuntu_simple
 ln -s lmp_ubuntu_simple lammps
 popd
@@ -16,7 +15,7 @@ pushd mylammps/src
 make -j 2 makeshlib && make -j 2 -f Makefile.shlib ubuntu_simple
 popd
 # install LAMMPS python wrapper
-pushd mylammps/python 
+pushd mylammps/python
 sudo python install.py /usr/lib/ $VIRTUAL_ENV/lib/python2.7/site-packages/
 popd
 python check_lammps_python.py
