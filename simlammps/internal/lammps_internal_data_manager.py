a920c1eeed7fc17aca7edd3266c0ac6df7e179b4
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-09-02 17:17:26 +0200

    Fix docstring

--- simlammps/internal/lammps_internal_data_manager.py
+++ simlammps/internal/lammps_internal_data_manager.py
@@ -226,8 +226,7 @@ class LammpsInternalDataManager(ABCDataManager):
 
         """
         if particle.uid not in self._particles[uname]:
-            self._add_atom(particle, uname)
-            return particle.uid
+            return self._add_atom(particle, uname)
         else:
             raise ValueError(
                 "particle with same uid ({}) alread exists".format(
@@ -372,7 +371,7 @@ class LammpsInternalDataManager(ABCDataManager):
                                                particle.uid)
 
     def _add_atom(self, particle, uname):
-        """ Add a atom at point's position to lammps
+        """ Add a atom to lammps
 
         If particle has uid equal to NONE, we will give
         it an uuid
@@ -387,8 +386,8 @@ class LammpsInternalDataManager(ABCDataManager):
 
         Returns
         -------
-        int :
-            lammps-id of added atom
+        uuid :
+            uid of added particle
 
         """
         coordinates = ' '.join(map(str, particle.coordinates))
