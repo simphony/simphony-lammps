b34d0115d5aaabdac55b515317844116c8d20f72
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-10-15 11:30:55 +0200

    Improve documentation related to atom styles

--- simlammps/common/atom_style_description.py
+++ simlammps/common/atom_style_description.py
@@ -4,16 +4,19 @@ from simlammps.common.atom_style import AtomStyle
 
 
 class ValueInfo(object):
-    """  Class describes cuba value and how to convert between LAMMPS/SIMPHONY
+    """  Class describes cuba value
+
+    Class information on cuba value and provides conversion
+    between LAMMPS/SIMPHONY
 
     Attributes
     ----------
     cuba_key : CUBA
         CUBA key
-    convertToCuba : function (optional)
-        method to convert from LAMMPS value to SimPhoNy CUBA
-    convertFromCuba : function (optional)
-        method to convert from SimPhoNy CUBA to LAMMPS value
+    convert_to_cuba : function (optional)
+        method to convert from LAMMPS value to SimPhoNy-CUBA
+    convert_from_cuba : function (optional)
+        method to convert from SimPhoNy-CUBA to LAMMPS value
 
     """
     def __init__(self,
@@ -24,7 +27,10 @@ class ValueInfo(object):
         self.convert_to_cuba = convert_to_cuba
         self.convert_from_cuba = convert_from_cuba
 
-
+# description of each atom-style:  each atom style has a particular set
+# of attributes that it supports/provides. For each atom style, a list of what
+# attributes it contains is listed. Note that the order of items in each lists
+# corresponds to the order they appear in lammps-data file
 ATOM_STYLE_DESCRIPTIONS = {
     AtomStyle.ATOMIC: [],  # just default (i.e. coordinates, velocity..)
     AtomStyle.GRANULAR:
