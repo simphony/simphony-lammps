35c9b635d74e7f521bf65329b05fbd5f91be7d70
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-11-04 18:01:51 +0100

    Reworked example so that using gui is optional

--- examples/dem_billiards/README.txt
+++ examples/dem_billiards/README.txt
@@ -1,7 +1,6 @@
-This is an early example of using DEM in simphony (simphony-lammps).
+This is an example of using DEM in simphony (simphony-lammps).
 
-It uses ligghts.  There should be a executable called "ligghts" in your path.  It also uses simphony-aviz.
-
+It uses ligghts. There should be a executable called "ligghts" in your path. It also uses simphony-aviz for visualization (if "--show" flag is used).
 
 cd simphony-lammps-md/examples/dem_billiards
 python dem_billiards.py
--- examples/dem_billiards/dem_billiards.py
+++ examples/dem_billiards/dem_billiards.py
@@ -1,38 +1,48 @@
 from __future__ import print_function
 
+import click
+
 from simphony.engine import lammps
-from simphony.visualisation import aviz
 
 from simlammps import EngineType
 from simphony.core.cuba import CUBA
 
 
-particles_list = lammps.read_data_file("billiards_init.data")
+@click.command()
+@click.option('--show/--no-show', default=False)
+def billiards_example(show):
+    particles_list = lammps.read_data_file("billiards_init.data")
 
-# The total number
-number_NVE_cycles = 1000
+    # configure dem-wrapper
+    dem = lammps.LammpsWrapper(engine_type=EngineType.DEM)
 
-# configure dem-wrapper
-dem = lammps.LammpsWrapper(engine_type=EngineType.DEM)
+    dem.CM_extension[lammps.CUBAExtension.THERMODYNAMIC_ENSEMBLE] = "NVE"
+    dem.CM[CUBA.NUMBER_OF_TIME_STEPS] = 1000
+    dem.CM[CUBA.TIME_STEP] = 0.001
+    total_steps = 35000
 
-dem.CM_extension[lammps.CUBAExtension.THERMODYNAMIC_ENSEMBLE] = "NVE"
-dem.CM[CUBA.NUMBER_OF_TIME_STEPS] = 1000
-dem.CM[CUBA.TIME_STEP] = 0.001
-total_time_steps = 35000
+    # Define the BC component of the SimPhoNy application model:
+    dem.BC_extension[lammps.CUBAExtension.BOX_FACES] = ["fixed",
+                                                        "fixed",
+                                                        "fixed"]
+    dem.add_dataset(particles_list[0])
+    particles_in_wrapper = dem.get_dataset(particles_list[0].name)
 
-# Define the BC component of the SimPhoNy application model:
-dem.BC_extension[lammps.CUBAExtension.BOX_FACES] = ["fixed",
-                                                    "fixed",
-                                                    "fixed"]
-dem.add_dataset(particles_list[0])
-particles_in_wrapper = dem.get_dataset(particles_list[0].name)
+    if show:
+        from simphony.visualisation import aviz
 
-#  set the proper view parameters for the snapshot
-aviz.show(particles_in_wrapper)
+        #  set the proper view parameters for the snapshot
+        aviz.show(particles_in_wrapper)
 
-step = 0
-for run_number in range(0, total_time_steps/dem.CM[CUBA.NUMBER_OF_TIME_STEPS]):
-    step = step + dem.CM[CUBA.NUMBER_OF_TIME_STEPS]
-    print ("running step {} of {}".format(step, total_time_steps))
-    dem.run()
-    aviz.snapshot(particles_in_wrapper, "billiards{:0>5}.png".format(step))
+    step = 0
+    for run_number in range(0, total_steps/dem.CM[CUBA.NUMBER_OF_TIME_STEPS]):
+        step = step + dem.CM[CUBA.NUMBER_OF_TIME_STEPS]
+        print ("running step {} of {}".format(step, total_steps))
+        dem.run()
+
+        if show:
+            aviz.snapshot(particles_in_wrapper,
+                          "billiards{:0>5}.png".format(step))
+
+if __name__ == '__main__':
+    billiards_example()
